ce8fe1bc2e665c5ba07091d2da024e1d7df354ed
swift <ldw871102@gmail.com>
2015-07-06 17:58:03 +0800

    Hellogit
    test dateformat

--- VelocityTest_0104/src/HelloGit.java
+++ VelocityTest_0104/src/HelloGit.java
@@ -1,5 +1,14 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class HelloGit {
 
-	fedgtfg
+	public static void main(String[] args) {
+		String id = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		System.out.println(id);
+		
+		String idd = "112233";
+		System.out.println(idd.substring(0,idd.length()-2));
+	}
 }
