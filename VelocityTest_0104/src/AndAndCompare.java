3e511ef542c0a3d228318f1b4399d5ba82ca7eb8
swift <ldw871102@gmail.com>
2015-07-14 13:45:02 +0800

    &&
    && compare

---
+++ VelocityTest_0104/src/AndAndCompare.java
@@ -0,0 +1,20 @@
+
+public class AndAndCompare {
+	public static void main(String[] args) {
+		String b=null;
+		if(b.equals("null")){
+			System.out.println("if");
+		}else {
+			System.out.println("else");
+		}
+	}
+	
+}
+
+
+class Hello{
+	public static double  test() {
+		System.out.println("java");
+		return Math.random();
+	}
+}
